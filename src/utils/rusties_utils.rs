1bd8f7c9314c1c54a7950a2afdd31926a31b166d
John Doe <johndoe@example.com>
2022-01-04 03:32:36 +0000

    added second function - not yet working

--- src/utils/rusties_utils.rs
+++ src/utils/rusties_utils.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 
 pub fn len_rusties(rustie: &str) -> usize {
     /*
@@ -8,6 +10,23 @@ pub fn len_rusties(rustie: &str) -> usize {
 
     return rustie.matches('[').count() + rustie.matches('.').count();
 }
+
+
+pub fn split_rusties(rustie: &str) -> Vec<&str> {
+    // :param rustie: a rusties string.
+    // :return: the symbols of the rusties string one-by-one with order preserved
+    // needs lots of work
+    
+    let re = Regex::new(r"\[[\w]\]|\[.\w\]|\.").unwrap();
+    //let references: Vec<&str> = strings.iter().collect();
+    let mut references: Vec<&str> = Vec::new();
+    for cap in re.captures_iter(rustie) {
+        references.push(cap); 
+}
+    return references; 
+}
+
+
 
 #[test]
 fn test_len_rusties() {
